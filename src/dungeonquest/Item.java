81165efa75a4ff5bf9a0d1bc48ad62be193dba2b
jviravouth <77465751+jviravouth@users.noreply.github.com>
2022-01-27 17:30:45 -0500

    Initial Sprint (1/26/22)
    - Created Room Class
    - Created Item Class
    - Started Entrance to Dungeon

---
+++ src/dungeonquest/Item.java
@@ -0,0 +1,20 @@
+package dungeonquest;
+
+import java.util.Hashtable;
+
+public class Item {
+    Hashtable<String, String> itemDict = new Hashtable<String, String>();
+    private String itemName;
+    private String itemDesc;
+    
+    
+    public Item(String name, String desc) {
+        itemName = name;
+        itemDesc = desc;
+        itemDict.put(this.itemName, this.itemDesc);
+    }
+    
+    public void getItem() {
+        System.out.println(itemDict.get(this.itemName));
+    }
+}
---
+++ src/dungeonquest/Room.java
@@ -0,0 +1,46 @@
+package dungeonquest;
+
+import java.util.*;
+
+
+public class Room {
+    // Variables
+    private String roomName;
+    private String roomDescription;
+    private String roomDeathState;
+    private String roomDoorNumber;
+    private String roomDoorDesc;
+    private int turnNumber = 0;
+    static Hashtable<String, String> itemDict = new Hashtable<String, String>();
+    
+    
+    // Constructor
+    public Room (String name, String desc, String door, String doorDesc, String deathState) {
+        roomName = name;
+        roomDescription = desc;
+        roomDoorNumber = door;
+        roomDeathState = deathState;
+        roomDoorDesc = doorDesc;
+    }
+    
+    // Add object to objectDict
+    public void addObject(String itemName, String itemDesc) {
+        itemDict.put(itemName, itemDesc);
+    }
+    
+    // Remove object from itemDict
+    public void removeObject(String itemName) {
+        itemDict.remove(itemName);
+    }
+    
+    // Method to return itemDict
+    public void returnObjectDict (String objectNum) {
+        System.out.println(itemDict.get(objectNum));
+    }
+    
+    // Method to return roomDeathState description
+    public String deathState () {
+        return roomDeathState;
+    }
+    
+}
